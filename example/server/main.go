fcd93038e151952f94a88b10ab29c61efe8187e3
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-06-08 13:13:01 +0900

    set SentryRavenInterceptor in the Echo example service

--- example/server/main.go
+++ example/server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"github.com/pkg/errors"
 	"log"
 	"net"
 
 	pb "github.com/sat0yu/sentry-raven-grpc/example/mirror"
+	raven "github.com/sat0yu/sentry-raven-grpc"
 	"google.golang.org/grpc"
 )
 
@@ -18,6 +20,12 @@ type server struct{}
 // Echo implements mirror.Mirror
 func (s *server) Echo(ctx context.Context, in *pb.EchoRequest) (*pb.EchoResponse, error) {
 	log.Printf("Received: %v", in.RequestBody)
+
+	if len(in.RequestBody) > 5 {
+		log.Printf("too long! the error is reported to Sentry")
+		return nil, errors.New("too long")
+	}
+
 	var result []byte
 	for _, ch := range []byte(in.RequestBody) {
 		result = append([]byte{ch}, result...)
@@ -30,7 +38,12 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
+
+	dsn := "https://be6950a062634bc2a2ec35d53a493f8f:f9a9ae305d3941e393e19702e3f1ca9e@sentry.io/1477707"
+	ravenClient := raven.NewClient(dsn)
+	option := raven.SentryRavenInterceptorOption(ravenClient)
+	s := grpc.NewServer(option)
+
 	pb.RegisterMirrorServer(s, &server{})
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/certifi/gocertifi v0.0.0-20190506164543-d2eda7129713 // indirect
 	github.com/getsentry/raven-go v0.2.0
 	github.com/golang/protobuf v1.3.1
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pkg/errors v0.8.1
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
 	google.golang.org/appengine v1.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20190605220351-eb0b1bdb6ae6 // indirect
