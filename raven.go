e8da35fe26ca318fc63979d8f1b89126382f075c
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-06-08 18:52:33 +0900

    stop exposing some functions being unnecessarily public

--- raven.go
+++ raven.go
@@ -4,19 +4,19 @@ import (
 	"github.com/getsentry/raven-go"
 )
 
-// RavenClient wraps the original raven client
-type RavenClient struct {
+// ravenClient wraps the original raven client
+type ravenClient struct {
 	*raven.Client
 }
 
 // CaptureError calls the original function which provided by the raven package
-func (rc RavenClient) CaptureError(err error, tags map[string]string) string {
+func (rc ravenClient) CaptureError(err error, tags map[string]string) string {
 	return rc.Client.CaptureError(err, tags)
 }
 
 // NewClient injects the raven package
-func NewClient(dsn string) RavenClient {
+func NewClient(dsn string) ravenClient {
 	c := raven.DefaultClient
 	c.SetDSN(dsn)
-	return RavenClient{c}
+	return ravenClient{c}
 }
